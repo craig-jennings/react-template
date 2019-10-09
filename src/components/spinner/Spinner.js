23b1c9fcd3a39fa6f144774786aef54417b95e68
Craig Jennings <craig.jennings11@gmail.com>
2019-10-09 09:34:49 -0500

    Update dependencies

--- src/components/spinner/Spinner.js
+++
@@ -1,5 +0,0 @@
-import 'wc-spinners/dist/hollow-dots-spinner';
-
-const Spinner = () => <hollow-dots-spinner />;
-
-export default Spinner;
--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import modal from './reducers/modal';
 import notifications from './reducers/notifications';
-import spinner from './reducers/spinner';
 
 export default combineReducers({
-  isSpinnerVisible: spinner,
   modal,
   notifications,
 });
